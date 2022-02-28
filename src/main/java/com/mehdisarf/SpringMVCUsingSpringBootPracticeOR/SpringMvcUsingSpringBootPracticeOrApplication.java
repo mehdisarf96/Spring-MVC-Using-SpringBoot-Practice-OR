8d9d8b580b3321cdf54b45010d3b499bfcb722fb
M.Mehdi <m.sfj9696@gmail.com>
2022-02-28 17:27:17 +0330

    Init Commit

---
+++ src/main/java/com/mehdisarf/SpringMVCUsingSpringBootPracticeOR/SpringMvcUsingSpringBootPracticeOrApplication.java
@@ -0,0 +1,13 @@
+package com.mehdisarf.SpringMVCUsingSpringBootPracticeOR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcUsingSpringBootPracticeOrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcUsingSpringBootPracticeOrApplication.class, args);
+	}
+
+}
