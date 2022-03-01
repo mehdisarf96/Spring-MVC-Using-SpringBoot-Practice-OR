ebe6956b1ae909b26d883458f1770c86bd8a55e1
M.Mehdi <m.sfj9696@gmail.com>
2022-03-01 15:42:08 +0330

    create WAR file from my Spring Boot project.

--- src/main/java/com/mehdisarf/SpringMVCUsingSpringBootPracticeOR/SpringMvcUsingSpringBootPracticeOrApplication.java
+++ src/main/java/com/mehdisarf/SpringMVCUsingSpringBootPracticeOR/SpringMvcUsingSpringBootPracticeOrApplication.java
@@ -2,9 +2,11 @@ package com.mehdisarf.SpringMVCUsingSpringBootPracticeOR;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringMvcUsingSpringBootPracticeOrApplication {
+public class SpringMvcUsingSpringBootPracticeOrApplication
+        extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMvcUsingSpringBootPracticeOrApplication.class, args);
