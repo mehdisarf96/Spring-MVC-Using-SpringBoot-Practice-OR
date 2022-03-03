27320e164cd324a693199368cadb78fd7fff98bc
M.Mehdi <m.sfj9696@gmail.com>
2022-03-03 18:53:00 +0330

    Add some details to the @RequestMapping annotations.

--- src/main/java/com/mehdisarf/SpringMVCUsingSpringBootPracticeOR/controller/ProjectController.java
+++ src/main/java/com/mehdisarf/SpringMVCUsingSpringBootPracticeOR/controller/ProjectController.java
@@ -2,13 +2,35 @@ package com.mehdisarf.SpringMVCUsingSpringBootPracticeOR.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
 
-    @RequestMapping("/add")
-    public String addProject(){
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String addProject() {
+        System.out.println("Invoking add project.");
         return "project_add";
     }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String saveProject() {
+        System.out.println("Invoking save project.");
+        return "project_add";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST, params = {"type=multi"})
+    public String yearProject() {
+        System.out.println("Invoking year project.");
+        return "project_add";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST, params = {"type=multi","special"})
+    public String specialProject() {
+        System.out.println("Invoking special project.");
+        return "project_add";
+    }
+
+
 }
