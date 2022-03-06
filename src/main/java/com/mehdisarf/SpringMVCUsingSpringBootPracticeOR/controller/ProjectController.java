a6f3ae776c8b130c4ac675ecf55ad32adc322273
M.Mehdi <m.sfj9696@gmail.com>
2022-03-06 05:38:43 +0330

    Autowire services into our Controllers.

--- src/main/java/com/mehdisarf/SpringMVCUsingSpringBootPracticeOR/controller/ProjectController.java
+++ src/main/java/com/mehdisarf/SpringMVCUsingSpringBootPracticeOR/controller/ProjectController.java
@@ -1,12 +1,28 @@
 package com.mehdisarf.SpringMVCUsingSpringBootPracticeOR.controller;
 
+import com.mehdisarf.SpringMVCUsingSpringBootPracticeOR.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
+
+    private final ProjectService projectService;
+
+    @Autowired
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @RequestMapping(value = "/find")
+    public String find(Model model) {
+        model.addAttribute("projects", projectService.findAll());
+        return "projects";
+    }
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String addProject() {
@@ -26,11 +42,9 @@ public class ProjectController {
         return "project_add";
     }
 
-    @RequestMapping(value = "/add", method = RequestMethod.POST, params = {"type=multi","special"})
+    @RequestMapping(value = "/add", method = RequestMethod.POST, params = {"type=multi", "special"})
     public String specialProject() {
         System.out.println("Invoking special project.");
         return "project_add";
     }
-
-
 }
