779ee42f4aca03da7f1bf3774f5b2c431195023a
M.Mehdi <m.sfj9696@gmail.com>
2022-03-06 05:15:27 +0330

    Add and use a model.

--- src/main/java/com/mehdisarf/SpringMVCUsingSpringBootPracticeOR/controller/MainController.java
+++ src/main/java/com/mehdisarf/SpringMVCUsingSpringBootPracticeOR/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.mehdisarf.SpringMVCUsingSpringBootPracticeOR.controller;
 
+import com.mehdisarf.SpringMVCUsingSpringBootPracticeOR.entity.Project;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -9,7 +11,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MainController {
 
     @RequestMapping("/")
-    public String greeting() {
+    public String greeting(Model model) {
+
+        Project project = new Project();
+        project.setName("Mehdi's Spring MVC Project");
+        project.setSponsor("Mehdi Sarf");
+        project.setDescription("This is my MVC project.");
+
+        model.addAttribute("myCurrentProject", project);
+
         return "home";
     }
 }
